ff0634f45d9d41a92c8d3eb8560e45ce9cdd8495
RamShankarKumar <ramshankarkumar07@gmail.com>
2021-12-29 13:00:28 +0530

    make responsive

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const loginClose = document.querySelector('.login-close');
 const signupClose = document.querySelector('.signup-close');
 const loginOptionButton = document.querySelector('.login-option-button');
 const signupOptionButton = document.querySelector('.signup-option-button');
+const buttons = document.querySelector('.buttons');
 
 
 
@@ -14,32 +15,54 @@ const signupOptionButton = document.querySelector('.signup-option-button');
 
 // showing modal when clicked on login or signup button.
 const loginClickHandler = () => {
-    loginModal.classList.toggle('login-modal-active');
+    if(!loginModal.classList.contains('login-modal-active')){
+        loginModal.classList.add('login-modal-active');
+        buttons.classList.add('buttons-disable');
+    }
 }
-login.addEventListener('click', loginClickHandler);
 
 const signupClickHandler = () => {
-    signupModal.classList.toggle('signup-modal-active');
+    if(!signupModal.classList.contains('signup-modal-active')){
+        signupModal.classList.add('signup-modal-active');
+        buttons.classList.add('buttons-disable');
+    }
 }
+
+const loginCloseClickHandler = () => {
+    if(loginModal.classList.contains('login-modal-active')){
+        loginModal.classList.remove('login-modal-active');
+        buttons.classList.remove('buttons-disable');
+    }
+}
+
+const signupCloseClickHandler = () => {
+    if(signupModal.classList.contains('signup-modal-active')){
+        signupModal.classList.remove('signup-modal-active');
+        buttons.classList.remove('buttons-disable');
+    }
+}
+
+// opening modal with login or signup buttons
+login.addEventListener('click', loginClickHandler);
 signup.addEventListener('click', signupClickHandler);
 
-
-
-
-
 // closing modal with close button
-loginClose.addEventListener('click', loginClickHandler);
-signupClose.addEventListener('click', signupClickHandler);
+loginClose.addEventListener('click', loginCloseClickHandler);
+signupClose.addEventListener('click', signupCloseClickHandler);
 
 
 // changing modal with option button.
 const loginOptionClickHandler = () => {
-    signupClickHandler();
-    loginClickHandler();
+    if(signupModal.classList.contains('signup-modal-active')){
+        signupModal.classList.remove('signup-modal-active');
+        loginModal.classList.add('login-modal-active');
+    }
 }
 const signupOptionClickHandler = () => {
-    loginClickHandler();
-    signupClickHandler();
+    if(loginModal.classList.contains('login-modal-active')){
+        loginModal.classList.remove('login-modal-active');
+        signupModal.classList.add('signup-modal-active');
+    }
 }
 loginOptionButton.addEventListener('click', loginOptionClickHandler);
 signupOptionButton.addEventListener('click', signupOptionClickHandler);
